baa51e5a2ac0e3168adac257c3e9e5e2557397d6
Davis84 <58550405+Davis84@users.noreply.github.com>
2020-01-15 21:37:01 +0100

    Add files via upload

--- Main.js
+++ Main.js
@@ -7,9 +7,29 @@ const pug = require('pug');
 
 
 
+
+
+app.set('view engine', 'pug');
+app.get('/', function (req, res) {
+	res.render('oui.pug', { ok: "ca marche ou pas?" });
+});
+
 // Compile the source code
 const compiledFunction = pug.compileFile('views/oui.pug');
 
 const server = app.listen(7020, () => {
   console.log(`Express running → PORT ${server.address().port}`);
+ });
+ 
+
+app.get('/sujet1', function (req, res) {
+	res.render('sujet1.pug');
+});
+
+app.get('/sujet2', function (req, res) {
+	res.render('sujet2.pug');
+});
+
+app.get('/sujet3', function (req, res) {
+	res.render('sujet3.pug');
 });
